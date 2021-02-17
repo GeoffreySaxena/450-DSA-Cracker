6d910361dbde02b3b346992c9c85edb1c6921ca5
Pratik Geoffrey Saxena <54907139+GeoffreySaxena@users.noreply.github.com>
2021-02-17 12:30:29 +0530

    Create Reverse a String.cpp

---
+++ Arrays/Reverse a String.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string str);
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+string reverseWord(string str){
+
+   for (int i=str.length()-1; i>=0; i--) 
+      cout << str[i];
+}
